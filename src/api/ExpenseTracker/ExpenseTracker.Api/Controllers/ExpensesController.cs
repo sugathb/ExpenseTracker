5cd675dc13551eae6d8683f0adae83975aa4c95d
sugathb <sugathb@gmail.com>
2021-08-20 10:52:01 +1000

    expenses-controller-added

---
+++ src/api/ExpenseTracker/ExpenseTracker.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExpensesController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello..");
+        }
+    }
+}
